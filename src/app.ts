dc61ddff83fe58e96026bf1ac215e6e15fefe12a
Kiffeur <emmanueldesong46@gmail.com>
2024-11-29 17:25:51 +0100

    entrées serveur

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,15 @@ import { AuthController } from './controllers/auth.controller';
 import sequelize from './config/database';
 import User from './models/User';
 const app = express();
+const cors = require('cors');
 const authController = new AuthController();
 
 app.use(express.json());
+//Middleware CORS pour autoriser toutes les origines
+app.use(cors());
+
+// Pour s'assurer que les requêtes préliminaires (OPTIONS) fonctionnent
+app.options('*', cors());
 
 app.post('/auth/register', (req, res) => authController.signup(req, res));
 app.post('/auth/login', (req, res) => authController.signin(req, res));
